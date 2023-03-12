9794a690ec16819dd79c5b47eaedc28255612e8e
Lev Rusanov <30170278+JDM170@users.noreply.github.com>
2023-03-12 16:11:19 +0700

    Первый коммит и промежуточный результат
    Signed-off-by: Lev Rusanov <30170278+JDM170@users.noreply.github.com>

---
+++ Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_start = new System.Windows.Forms.Button();
+            this.btn_stop = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lbl_pass_count = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lbl_err_count = new System.Windows.Forms.Label();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn_start
+            // 
+            this.btn_start.Location = new System.Drawing.Point(30, 26);
+            this.btn_start.Name = "btn_start";
+            this.btn_start.Size = new System.Drawing.Size(75, 23);
+            this.btn_start.TabIndex = 0;
+            this.btn_start.Text = "Старт";
+            this.btn_start.UseVisualStyleBackColor = true;
+            this.btn_start.Click += new System.EventHandler(this.btn_start_Click);
+            // 
+            // btn_stop
+            // 
+            this.btn_stop.Location = new System.Drawing.Point(136, 26);
+            this.btn_stop.Name = "btn_stop";
+            this.btn_stop.Size = new System.Drawing.Size(75, 23);
+            this.btn_stop.TabIndex = 1;
+            this.btn_stop.Text = "Стоп";
+            this.btn_stop.UseVisualStyleBackColor = true;
+            this.btn_stop.Click += new System.EventHandler(this.btn_stop_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(30, 67);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(306, 31);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(30, 120);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Пройдено:";
+            // 
+            // lbl_pass_count
+            // 
+            this.lbl_pass_count.AutoSize = true;
+            this.lbl_pass_count.BackColor = System.Drawing.SystemColors.Control;
+            this.lbl_pass_count.Location = new System.Drawing.Point(92, 120);
+            this.lbl_pass_count.Name = "lbl_pass_count";
+            this.lbl_pass_count.Size = new System.Drawing.Size(13, 13);
+            this.lbl_pass_count.TabIndex = 4;
+            this.lbl_pass_count.Text = "0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(30, 158);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(50, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Ошибок:";
+            // 
+            // lbl_err_count
+            // 
+            this.lbl_err_count.AutoSize = true;
+            this.lbl_err_count.Location = new System.Drawing.Point(92, 158);
+            this.lbl_err_count.Name = "lbl_err_count";
+            this.lbl_err_count.Size = new System.Drawing.Size(13, 13);
+            this.lbl_err_count.TabIndex = 6;
+            this.lbl_err_count.Text = "0";
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(179, 126);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(157, 45);
+            this.trackBar1.TabIndex = 7;
+            this.trackBar1.Visible = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(161, 158);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "медленнее";
+            this.label2.Visible = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(298, 158);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(50, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "быстрее";
+            this.label4.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(370, 204);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.lbl_err_count);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lbl_pass_count);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btn_stop);
+            this.Controls.Add(this.btn_start);
+            this.Name = "Form1";
+            this.Text = "Учимся печатать быстро";
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_start;
+        private System.Windows.Forms.Button btn_stop;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lbl_pass_count;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lbl_err_count;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+/*using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;*/
+using System.Windows.Forms;
+
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        static int timerStart = 2000;
+        string str = "";
+        int lastSymbol = 0;
+        int pass_count = 0;
+        int err_count = 0;
+        Random rnd = new Random();
+        Timer timer = new Timer();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void timerCallback(object sender, EventArgs ea)
+        {
+            lastSymbol = rnd.Next(1072, 1103);
+            if (str.Length < 10)
+            {
+                str += (char)lastSymbol;
+                textBox1.Text = str;
+            }
+            else
+            {
+                timer.Stop();
+                MessageBox.Show("Игра окончена");
+            }
+        }
+
+        private void btn_start_Click(object sender, EventArgs e)
+        {
+            pass_count = 0;
+            err_count = 0;
+            lbl_pass_count.Text = pass_count.ToString();
+            lbl_err_count.Text = err_count.ToString();
+            timer.Interval = timerStart;
+            timer.Tick += new EventHandler(timerCallback);
+            timer.Start();
+            textBox1.Focus();
+        }
+
+        private void btn_stop_Click(object sender, EventArgs e)
+        {
+            if(timer.Enabled) timer.Stop();
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = true;
+            if (timer.Enabled)
+            {
+                if (e.KeyChar == lastSymbol)
+                {
+                    str = str.Substring(0, str.Length - 1);
+                    pass_count++;
+                    textBox1.Text = str;
+                    lbl_pass_count.Text = pass_count.ToString();
+                }
+                else
+                {
+                    err_count++;
+                    lbl_err_count.Text = err_count.ToString();
+                }
+            }
+        }
+
+        /*private void trackBar1_ValueChanged(object sender, EventArgs e)
+        {
+            textBox2.Clear();
+            textBox2.Text += trackBar1.Value.ToString();
+        }*/
+    }
+}
